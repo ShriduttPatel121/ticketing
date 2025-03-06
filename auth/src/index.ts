dcc25fb5ea44fdb93a0784a9fd6d1988d99c670f
shridutt2810 <shridutt2810@gmail.com>
2025-03-06 22:57:22 +0530

    infra setup

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,10 @@ const app = express();
 
 app.use(json());
 
+app.get('/api/users/currentuser',(req, res) => {
+    res.send('Hi From Shridutt!!!28');
+})
+
 app.listen(3000, () => {
-    console.log("Auth Server started at 3000")
+    console.log("Auth Server started at 3000!!!!");
 })
