2f55e06ecea2ee048b86346b45054b49d0525557
shridutt2810 <shridutt2810@gmail.com>
2025-03-16 21:56:14 +0530

    improved type system

--- auth/src/controllers/signup.ts
+++ auth/src/controllers/signup.ts
@@ -1,14 +1,13 @@
-import express, { Request, RequestHandler, Response } from 'express';
+import { Request, RequestHandler, Response } from 'express';
 import { SignupSchema } from '../Types/SignupSchema';
 
-export const SignupController: RequestHandler  = async (req: Request, res: Response) => {
+export const SignupController: RequestHandler  = (async (req: Request, res: Response) => {
     const body = req.body;
     const parsedData = SignupSchema.safeParse(body);
     if(parsedData.error) {
-        res.status(411).json({
+        return res.status(411).json({
             message: 'Incorrect payload!!!',
             error: parsedData.error
         });
-        return;
     }
-}
+}) as RequestHandler;
