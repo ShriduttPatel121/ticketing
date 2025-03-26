c4bdac0843517387afa1f6021c15766d95094501
shridutt2810 <shridutt2810@gmail.com>
2025-03-26 21:10:03 +0530

    error handling code

--- auth/src/middleware/errorHandler.ts
+++ auth/src/middleware/errorHandler.ts
@@ -1,9 +1,10 @@
 import { Request, Response, NextFunction } from 'express'
+import { EStatusCodes } from '../utils/StatusCodes';
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
     console.error('something went wrong!! ', err);
 
-    res.status(500).json({
+    res.status(EStatusCodes.SERVER_ERROR).json({
         message: 'something went wrong'
     })
 }
