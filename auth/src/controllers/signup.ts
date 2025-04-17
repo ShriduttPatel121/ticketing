89540a9591b78f23418231df6926e59596695ee0
shridutt2810 <shridutt2810@gmail.com>
2025-04-17 22:15:42 +0530

    signup added

--- auth/src/controllers/signup.ts
+++ auth/src/controllers/signup.ts
@@ -1,4 +1,4 @@
-import { Request, RequestHandler, Response } from 'express';
+import { Request, Response } from 'express';
 import { SignupSchema } from '../Types/SignupSchema';
 
 export const SignupController  = async (req: Request, res: Response) => {
@@ -6,7 +6,6 @@ export const SignupController  = async (req: Request, res: Response) => {
     const parsedData = SignupSchema.safeParse(body);
     if (parsedData.error) {
         return res.invalidPayload("Invalid Payload", parsedData.error);
-        
     }
 
     return res.created("User Created Successfully", parsedData.data);
